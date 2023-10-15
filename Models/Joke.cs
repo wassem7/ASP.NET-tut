bfdd8fa9b712d6e1cdb3b4b765e253c4678729f6
wassem7 <waseemdarkwa@gmail.com>
2023-10-15 03:40:12 +0000

    Joke Model Configuration

---
+++ Models/Joke.cs
@@ -0,0 +1,15 @@
+﻿namespace JokesWebApp.Models
+{
+    public class Joke
+    {
+
+        public int Id { get; set; }
+
+        public string JokesQuestion { get; set; }
+
+        public string JokesAnswer { get; set; }
+
+
+        public Joke() { }
+    }
+}
